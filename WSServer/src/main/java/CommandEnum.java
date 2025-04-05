294484bdfad22acdf227cce7ddac05c0b25b28d6
Jezreel Moraes <jezreel.moraes@asaas.com.br>
2025-04-05 12:18:08 -0300

    feat: Criando CommandEnum

---
+++ WSServer/src/main/java/CommandEnum.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.List;
+
+public enum CommandEnum {
+
+    USERS("/users", List.of("")),
+    CHOOSE_NAME("/choose-name", List.of("<your-name>")),
+    SEND_MESSAGE("/send-message", List.of("<user-name>", "<message>")),
+    SEND_FILE("/send-file", List.of("<user-name>", "<file-path>")),
+    EXIT("/exit", List.of(""));
+
+    private final String command;
+    private final List<String> arguments;
+
+    CommandEnum(String command, List<String> arguments) {
+        this.command = command;
+        this.arguments = arguments;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public static CommandEnum convert(String command) {
+        return Arrays.stream(CommandEnum.values())
+            .filter(commandEnum -> commandEnum.command.equals(command) )
+            .findFirst()
+            .orElse(null);
+    }
+
+    public static List<CommandEnum> listUserCommands() {
+        return Arrays.asList(
+            USERS,
+            SEND_MESSAGE,
+            SEND_FILE,
+            EXIT
+        );
+    }
+
+
+    public String getCommandHelp() {
+        return this.command + " " + String.join(" ", this.arguments);
+    }
+
+}
