f04b316645f49a8985a1fc145769ec3289874244
Gustavo Henrique da Trindade Martins <gustavomartins220@gmail.com>
2025-04-14 00:09:49 -0300

    url podendo ser configurada via args no jar

--- WSClient/src/main/java/org/client/Client.java
+++ WSClient/src/main/java/org/client/Client.java
@@ -8,17 +8,22 @@ import org.client.ui.NameSelectorFrame;
 import org.communication.Message;
 import org.communication.MessageType;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Client {
 
-    private static final int SERVER_PORT_NUMBER = 4000;
-    private static final String IP_LOCALHOST = "127.0.0.1";
+    private static final String DEFAULT_LOCAL_SERVER_ADDRESS = "127.0.0.1:4000";
+    private static final Pattern SERVER_ADDRESS_PATTERN = Pattern.compile("^[^:\\s]+:(\\d{1,5})$");
 
     public static Socket server;
     private static PrintStream output;
 
     public static void main(String[] args) {
         try {
-            serverConnect();
+            String serverAddress = loadServerAddress(args);
+
+            serverConnect(serverAddress);
             output = new PrintStream(server.getOutputStream(), true);
 
             new NameSelectorFrame(output);
@@ -27,11 +32,15 @@ public class Client {
         }
     }
 
-    private static void serverConnect() throws IOException {
+    private static void serverConnect(String serverAddress) throws IOException {
         try {
-            server = new Socket(IP_LOCALHOST, SERVER_PORT_NUMBER);
+            String[] parts = serverAddress.split(":");
+            String ip = parts[0];
+            int port = Integer.parseInt(parts[1]);
+
+            server = new Socket(ip, port);
             output = new PrintStream(server.getOutputStream());
-            System.out.println("Conectado com o servidor com sucesso");
+            System.out.println("Conectado com o servidor " + ip + ":" + port);
         } catch (Exception exception) {
             System.out.println("Erro ao conectar com servidor, erro: " + exception.getMessage());
             throw exception;
@@ -50,5 +59,12 @@ public class Client {
         }
 
         System.exit(0);
+    }
+
+    private static String loadServerAddress(String[] args) {
+        String inputAddress = args.length > 0 ? args[0] : DEFAULT_LOCAL_SERVER_ADDRESS;
+
+        Matcher matcher = SERVER_ADDRESS_PATTERN.matcher(inputAddress);
+        return matcher.matches() ? inputAddress : DEFAULT_LOCAL_SERVER_ADDRESS;
     }
 }
